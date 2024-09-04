a08d2141953e8e4e31e90fc493d08ccf4357e0d7
Your Name <rahulmohature0421@gmail.com>
2024-09-04 20:54:20 +0530

    new code added

--- src/home/ColorMaskPass.ts
+++ src/home/ColorMaskPass.ts
@@ -2,8 +2,22 @@ import GUI from 'lil-gui'
 import * as THREE from 'three'
 import { ShaderPass  } from 'three/examples/jsm/postprocessing/ShaderPass'
 import { shader } from './shaderChunk'
+import { uniform } from 'three/webgpu'
 
 
 export class ColorMaskPass{
-    
+    pass!: ShaderPass
+
+    constructor(private texture: THREE.Texture, private canvasAspect: number, private gui: GUI){
+        this.init()
+    }
+
+    private init = () =>{
+        const shader : THREE.Shader = {
+            uniforms : {
+                tDiffuse : { value : null },
+                
+            }
+        }
+    }
 }
