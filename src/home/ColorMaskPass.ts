0d0eb6485d9b1a1e4684f1e8fe47f225b48f5ef8
Your Name <rahulmohature0421@gmail.com>
2024-09-04 17:57:22 +0530

    new code added

---
+++ src/home/ColorMaskPass.ts
@@ -0,0 +1,9 @@
+import GUI from 'lil-gui'
+import * as THREE from 'three'
+import { ShaderPass  } from 'three/examples/jsm/postprocessing/ShaderPass'
+import { shader } from './shaderChunk'
+
+
+export class ColorMaskPass{
+    
+}
