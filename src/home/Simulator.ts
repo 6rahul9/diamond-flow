5a92363f038facc2353f8875167825466cb6c0e6
Your Name <rahulmohature0421@gmail.com>
2024-09-06 18:39:20 +0530

    .

--- src/home/Simulator.ts
+++ src/home/Simulator.ts
@@ -37,23 +37,29 @@ export class TCanvas extends TCanvasBase {
     private assets: Assets = {
         envMap : {
             path: publicPath('/resources/studio_small_08_1k.hdr') },
-		image: { path: publicPath('/resources/wlop5.jpg')
+		image: { path: publicPath('/resources/wlop5.jpg') }
         }
 
-        constructor(parentNode : parentNode ){
-            super(parentNode)
-
+    constructor(parentNode : parentNode ){
+        super(parentNode)
             this.loadAssets(this.assets).then(() => {
-                this.setScene()
-                this.createSimulator()
-                this.creatrLight()
-                this.createMesh()
-                this.createPostProcessing()
-                this.setResizeCallback()
-                this.addEvent()
-                this.setDispose()
-                this.animate(this.update)
-            })
-        }
+            this.setScene()
+            this.createSimulator()
+            this.creatrLight()
+            this.createMesh()
+            this.createPostProcessing()
+            this.setResizeCallback()
+            this.addEvent()
+            this.setDispose()
+            this.animate(this.update)
+        })
     }
+
+    private setScene = () => {
+        this.scene.background = new THREE.Color('#fefefe')
+        this.camera.position.set(0, 3, 15)
+        const controls = this.setOrbitControls(0.1)
+        controls.minAzimuthAngle = 0
+    }
+    
 }
