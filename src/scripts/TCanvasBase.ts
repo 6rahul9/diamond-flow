a0f585265e2536e3878335a49efe79b0930f234b
Your Name <rahulmohature0421@gmail.com>
2024-09-13 02:19:20 +0530

    bored with writing the code

--- src/scripts/TCanvasBase.ts
+++ src/scripts/TCanvasBase.ts
@@ -36,7 +36,32 @@ export abstract class TCanvasBase{
     constructor(parentNode: ParentNode, containerClassName = 'three-container'){
         let container: HTMLDivElement | null
         try{
-            cnstainer = parentNode 
+            container = parentNode .querySelector<HTMLDivElement>(`${containerClassName}`)
+            if(!container)throw new Error(`undefined container : (${containerClassName}`)
+        }catch(e){
+            console.error(e)
+            throw(e)
         }
+        this.container = container!
+        this.init()
+        this.addEvents()
+    }
+
+    // ------------------------------------------------------
+	// initialize
+
+    private init = () => {
+        const{width, height, aspect} = this.size
+        //renderer
+        this.renderer = THREE.WebGLRenderer({ antialias: true, alpha: true })
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.renderer.setSize(width, height)
+        this.renderer.shadowMap.enabled = true
+        this.renderer.outputEncoding = THREE.sRGBEncoding
+
+        // ↓ with unreal bloom effect
+		// renderer.toneMapping = THREE.ACESFilmicToneMapping
+		// append canvas element
+        
     }
 }
