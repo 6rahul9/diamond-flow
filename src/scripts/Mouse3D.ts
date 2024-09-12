0e5433fd21148df7db4ac45fe4515b6eafacb68f
Your Name <rahulmohature0421@gmail.com>
2024-09-12 18:01:25 +0530

    new code added

--- src/scripts/Mouse3D.ts
+++ src/scripts/Mouse3D.ts
@@ -9,4 +9,13 @@ export class Mouse3d{
         window.addEventListener('mousemove', this.handleMouseMove)
         window.addEventListener('touchmove', this.handleMouseMove)
     }
+
+    get position(){
+        this.camera.updateMatrixWorld()
+        this.ray.origin.setFromMatrixPosition(this.camera.matrixWorld)
+        this.ray.direction.set(this.mouse.x, this.mouse.y, 0.5).unproject(this.camera).sub(this.ray.origin).normalize()
+        const distance = this.ray.origin.length() / Math.cos(Math.PI - this.ray.direction.angleTo(this.ray.origin))
+        this.ray.origin.add(this.ray.direction.multiplyScalar(distance * 1.0))
+        return this.ray.origin;
+    }
 }
