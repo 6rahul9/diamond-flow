23a617d78b6d67f4ef200d9c43b3d101451b0b1f
Your Name <rahulmohature0421@gmail.com>
2024-09-12 20:23:08 +0530

    imported all te files

---
+++ src/scripts/TCanvasBase.ts
@@ -0,0 +1,9 @@
+import GUI from 'lil-gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+
