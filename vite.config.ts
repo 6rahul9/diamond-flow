e276f73c148cd9243c8436d394d4da3069238912
Your Name <rahulmohature0421@gmail.com>
2024-09-18 01:41:11 +0530

    did some changes, and the code is running beautifully

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,47 @@
-import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react';
 
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
 // export default defineConfig({
-//   plugins: [react()],
-// })
+//   plugins: [react()]
+// });
 
-export default defineConfig({
-  plugins: [react()]
-});
+
+import path from 'path';
+import { defineConfig, Plugin } from 'vite';
+import glsl from 'vite-plugin-glsl';
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig(({ mode }) => {
+	console.log('⚓ ' + mode)
+	return {
+		root: './src',
+		base: mode === 'development' ? '/' : '/diamond-flow/',
+		publicDir: '../public',
+		plugins: [
+			glsl(),
+			handlebars({
+				partialDirectory: path.resolve(__dirname, './src/partials/')
+			}) as Plugin
+		],
+		build: {
+			outDir: '../dist',
+			rollupOptions: {
+				input: {
+					home: path.resolve(__dirname, './src/index.html')
+				}
+			},
+			cssCodeSplit: false,
+			emptyOutDir: true
+		},
+		server: {
+			host: true
+		}
+	}
+})
