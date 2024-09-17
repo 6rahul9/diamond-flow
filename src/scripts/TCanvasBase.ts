d8ebef2a92ebe0e64353ea12a277cea41dcba0a0
Your Name <rahulmohature0421@gmail.com>
2024-09-17 20:12:32 +0530

    trired of writing the code

--- src/scripts/TCanvasBase.ts
+++ src/scripts/TCanvasBase.ts
@@ -153,7 +153,7 @@ export abstract class TCanvasBase{
 
         await Promise.all(
             Object.values(assets).map(async v => {
-                const extension = this.getextension(v.path)
+                const extension = this.getExtension(v.path)
 
                 if(['jpg', 'png', 'webp'].includes(extension)){
                     const texture = await textureLoader.loadAsync(v.path)
@@ -163,8 +163,27 @@ export abstract class TCanvasBase{
                 }else if(['glb'].includes(extension)){
                     const gltf = await gltfLoader.loadAsync(v.path)
                     v.data = gltf
+                }else if(['webm', 'mp4'].includes(extension)){
+                    const video = document.createElement('video')
+                    video.src = v.path
+                    video.muted = true
+                    video.loop = true 
+                    video.autoplay = true
+                    video.preload = 'auto'
+                    video.playsInline = true
+                    	// await video.play()
+                    const texture = new THREE.VideoTexture(video)
+                    texture.userData.aspect = video.videoWidth / video.videoHeight
+                    v.encoding && (texture.encoding = THREE.sRGBEncoding)
+                    v.data = texture
+                }else if(['hdr'].includes(extension)){
+                    const texture = await rgbeLoader.loadAsync(v.path)
+                    texture.mapping = THREE.EquirectangularReflectionMapping
+                    v.data = texture
                 }
             })
         )
     }
+
+    
 }
